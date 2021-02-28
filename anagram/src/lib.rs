9b67eecffe708bee0352d0e0e1c78f562c439dcf
Marcus Tenório <marcusvtenorio@gmail.com>
2021-02-28 01:46:59 -0300

    Anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,9 +1,28 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+use unicode_segmentation::UnicodeSegmentation;
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let word = &word.to_lowercase();
+
+    possible_anagrams
+        .iter()
+        .cloned()
+        .filter(|w| is_anagram(word, &w.to_lowercase()))
+        .collect()
+}
+
+fn is_anagram(word: &str, possible_anagram: &str) -> bool {
+    if word == possible_anagram {
+        return false;
+    }
+
+    get_sorted_graphemes(word) == get_sorted_graphemes(possible_anagram)
+}
+
+fn get_sorted_graphemes(word: &str) -> Vec<&str> {
+    let mut graphemes = word.graphemes(true).collect::<Vec<&str>>();
+    graphemes.sort_unstable();
+
+    graphemes
 }
