5c04641f31a83ab3a1f9a9ff304210a816749f6d
Marcus Tenório <marcusvtenorio@gmail.com>
2021-02-28 20:15:45 -0300

    Update anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -3,21 +3,26 @@ use std::collections::HashSet;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let word = &word.to_lowercase();
+    let word = word.to_lowercase();
+    let word_graphemes = get_sorted_graphemes(&word);
+    let word_hash = symmetric_fast_hash(&word);
 
     possible_anagrams
         .iter()
         .cloned()
-        .filter(|w| is_anagram(word, &w.to_lowercase()))
+        .filter(|w| {
+            if word.len() != w.len() {
+                return false;
+            }
+
+            let lw = w.to_lowercase();
+            if word == lw || word_hash != symmetric_fast_hash(&lw) {
+                return false;
+            }
+
+            word_graphemes == get_sorted_graphemes(&lw)
+        })
         .collect()
-}
-
-fn is_anagram(word: &str, possible_anagram: &str) -> bool {
-    if word == possible_anagram {
-        return false;
-    }
-
-    get_sorted_graphemes(word) == get_sorted_graphemes(possible_anagram)
 }
 
 fn get_sorted_graphemes(word: &str) -> Vec<&str> {
@@ -25,4 +30,14 @@ fn get_sorted_graphemes(word: &str) -> Vec<&str> {
     graphemes.sort_unstable();
 
     graphemes
+}
+
+pub fn symmetric_fast_hash(word: &str) -> u8 {
+    let mut hash: u8 = 0;
+
+    for byte in word.as_bytes() {
+        hash ^= byte;
+    }
+
+    hash
 }
