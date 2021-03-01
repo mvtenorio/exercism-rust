30f9c0d2d52c5e4f66c4411153677b9d7997fd32
Marcus Tenório <marcusvtenorio@gmail.com>
2021-03-01 18:52:19 -0300

    Update proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,15 +1,10 @@
-use std::iter;
-
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        return String::new();
-    }
-
     list.windows(2)
         .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
-        .chain(iter::once(format!(
-            "And all for the want of a {}.",
-            list[0]
-        )))
+        .chain(
+            list.iter()
+                .take(1)
+                .map(|i| format!("And all for the want of a {}.", i)),
+        )
         .collect()
 }
